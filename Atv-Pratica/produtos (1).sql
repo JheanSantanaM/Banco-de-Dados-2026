b25c26ca5787d6c33e768b5c1f3efa50ac0f1e22
Jhean Santana de Miranda <jheansmiranda@gmail.com>
2026-05-29 13:53:42 -0300

    Add files via upload

--- Atv-Pratica/produtos (1).sql
+++ Atv-Pratica/produtos (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 20-Maio-2026 às 22:57
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 7.4.29
+-- Tempo de geração: 29-Maio-2026 às 18:53
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,7 +43,8 @@ INSERT INTO `categoria` (`idcategoria`, `categoria`) VALUES
 (2, 'Laticínios'),
 (3, 'Açougue'),
 (4, 'Limpeza'),
-(5, 'molhos');
+(5, 'molhos'),
+(6, 'Roupas');
 
 -- --------------------------------------------------------
 
@@ -70,7 +71,19 @@ CREATE TABLE `produtos` (
 INSERT INTO `produtos` (`idproduto`, `produto`, `preco`, `qtde`, `datacad`, `ativo`, `idcat`, `observacao`, `marca`) VALUES
 (1, 'Acém Peça', '39.99', 50, '2026-05-13', b'1', 3, 'Sem gordura', ''),
 (2, 'Milho', '2.37', 10, '2026-10-01', b'0', 1, '', ''),
-(5, 'Patinho', '49.99', 30, '2026-07-23', b'1', 1, 'com gordura', 'friboi');
+(5, 'Patinho', '49.99', 30, '2026-07-23', b'1', 1, 'com gordura', 'friboi'),
+(6, 'Alcatra', '35.99', 20, '2026-08-30', b'1', 3, 'com gordura', 'friboi'),
+(7, 'Contra filé', '29.99', 33, '2026-07-29', b'1', 3, 'com gordura', 'friboi'),
+(8, 'Detergente', '3.99', 50, '2027-05-12', b'1', 4, 'Não contaminado', 'Ypê'),
+(9, 'Molho Tomate', '2.49', 73, '2026-12-27', b'1', 5, '100% Tomate', 'Fugini'),
+(10, 'Ervilha', '5.99', 80, '2026-09-11', b'1', 1, '170g', 'Quero'),
+(11, 'Sabão em pó', '13.29', 40, '2027-08-17', b'1', 4, 'Use em roupa', 'OMO'),
+(12, 'Leite', '4.59', 60, '2027-01-11', b'1', 2, '1 Litro', 'Parmalat'),
+(13, 'Leite', '3.99', 50, '2027-01-11', b'1', 2, '1 Litro', 'Italac'),
+(14, 'Leite Condensado', '6.49', 30, '2027-03-15', b'1', 2, 'Desnatado', 'Moça'),
+(15, 'Queijo', '13.49', 20, '2027-04-20', b'1', 2, '', 'Fusqueijão'),
+(16, 'Requeijão', '7.49', 40, '0000-00-00', b'1', 2, 'Cremoso', 'Vigor'),
+(17, 'Molho barbecue', '9.99', 0, '2027-04-20', b'0', 2, '', 'KiSabor');
 
 --
 -- Índices para tabelas despejadas
@@ -97,13 +110,13 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT de tabela `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `idproduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idproduto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Restrições para despejos de tabelas
