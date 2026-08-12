31d1f634454d08e9202acd9610be513fcc234a64
Jhean Santana de Miranda <jheansmiranda@gmail.com>
2026-08-12 17:50:51 -0300

    Add files via upload

--- hotel.sql
+++ hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 29/07/2026 às 22:50
+-- Tempo de geração: 12/08/2026 às 22:50
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -41,7 +41,7 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`codcliente`, `cliente`, `email`, `cpf`) VALUES
-(1, 'Jhean Santana', 'jheansantana@gmail.com', '478.864.578-54'),
+(1, 'Jhean Santana', 'jheansantana1201@gmail.com', '478.864.578-54'),
 (2, 'Flavio Gustavo', 'flaviogustavo@gmail.com', '564.880.086-76'),
 (3, 'Pietro Carvalho', 'pietrocarvalho@gmail.com', '458.056.458-44'),
 (4, 'Marcos Paulo', 'Marcospaulo@gmail.com', '533.786.568-34'),
@@ -64,6 +64,17 @@ CREATE TABLE `hospedagem` (
   `codquarto` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `hospedagem`
+--
+
+INSERT INTO `hospedagem` (`idhospedagem`, `dataentrada`, `datasaida`, `horaentrada`, `horasaida`, `totalhospedagem`, `codcli`, `codquarto`) VALUES
+(1, '2026-07-22', NULL, '12:54:03', NULL, NULL, 1, 12),
+(2, '2026-07-22', NULL, '13:00:07', NULL, NULL, 2, 13),
+(3, '2026-07-30', '2026-08-12', '15:20:43', '18:00:00', NULL, 3, 14),
+(4, '2026-08-01', NULL, '17:08:45', NULL, NULL, 4, 16),
+(5, '2026-07-31', NULL, '19:30:07', NULL, NULL, 5, 14);
+
 -- --------------------------------------------------------
 
 --
@@ -82,7 +93,7 @@ CREATE TABLE `quarto` (
 --
 
 INSERT INTO `quarto` (`codquarto`, `quarto`, `andar`, `tipo`) VALUES
-(10, '11', '1°andar', 'Familia - 3 camas'),
+(10, '11', '1°andar', 'Casal'),
 (11, '12', '1°andar', 'Familia - 4 camas'),
 (12, '13', '1°andar', 'Casal'),
 (13, '14', '1°andar', 'Casal'),
@@ -116,7 +127,8 @@ INSERT INTO `servico` (`codservico`, `servico`, `qtde`, `valor`, `tipo`) VALUES
 (3, 'Salgadinho Fandangus 250g', 0, 8.00, 'Comida'),
 (4, 'Strogonoff', 0, 20.00, 'Comida'),
 (5, 'Salgadinho Doritos 200g', 0, 10.00, 'Comida'),
-(6, 'Pepsi lata 350ml', 0, 11.50, 'Bebida');
+(6, 'Pepsi lata 350ml', 0, 11.50, 'Bebida'),
+(7, 'Picanha', 0, 54.00, 'Comida');
 
 -- --------------------------------------------------------
 
@@ -131,6 +143,32 @@ CREATE TABLE `servicoshospedagem` (
   `horaservico` time DEFAULT NULL,
   `total` decimal(7,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `servicoshospedagem`
+--
+
+INSERT INTO `servicoshospedagem` (`idhospedagem`, `idservico`, `dataservico`, `horaservico`, `total`) VALUES
+(1, 2, '2026-07-22', '19:40:39', NULL),
+(1, 3, '2026-07-22', '20:05:20', NULL),
+(1, 6, '2026-07-24', '12:30:45', NULL),
+(1, 4, '2026-07-24', '12:35:12', NULL),
+(2, 1, '2026-07-22', '21:03:30', NULL),
+(2, 5, '2026-07-22', '21:30:32', NULL),
+(2, 4, '2026-07-24', '12:20:13', NULL),
+(2, 2, '2026-07-24', '12:23:40', NULL),
+(3, 2, '2026-07-31', '09:43:00', NULL),
+(3, 1, '2026-07-31', '18:32:28', NULL),
+(3, 5, '2026-08-01', '16:35:39', NULL),
+(3, 6, '2026-08-02', '12:29:17', NULL),
+(4, 4, '2026-08-02', '13:27:40', NULL),
+(4, 1, '2026-08-02', '13:30:45', NULL),
+(4, 5, '2026-08-09', '22:08:34', NULL),
+(4, 6, '2026-08-02', '22:10:35', NULL),
+(5, 3, '2026-08-01', '14:45:43', NULL),
+(5, 6, '2026-08-01', '14:46:09', NULL),
+(5, 4, '2026-08-02', '11:30:22', NULL),
+(5, 2, '2026-08-02', '11:31:09', NULL);
 
 --
 -- Índices para tabelas despejadas
@@ -187,7 +225,7 @@ ALTER TABLE `cliente`
 -- AUTO_INCREMENT de tabela `hospedagem`
 --
 ALTER TABLE `hospedagem`
-  MODIFY `idhospedagem` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idhospedagem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `quarto`
@@ -199,7 +237,7 @@ ALTER TABLE `quarto`
 -- AUTO_INCREMENT de tabela `servico`
 --
 ALTER TABLE `servico`
-  MODIFY `codservico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `codservico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restrições para tabelas despejadas
